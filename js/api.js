20aec5a417d04b18940f037d952a157ca472a3d6
ReactRover <krolov0@gmail.com>
2024-09-12 23:08:42 +0300

    Ekran görüntüsü eklendi

--- js/api.js
+++ js/api.js
@@ -1,9 +1,52 @@
+import { renderSearchMusic, renderSongs } from "./ui.js";
+
+const options = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "2614167a39mshf3f9f0f1084be1ep1bc351jsn9c60381bd801",
+    "x-rapidapi-host": "shazam.p.rapidapi.com",
+  },
+};
+const optionsTop = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "2614167a39mshf3f9f0f1084be1ep1bc351jsn9c60381bd801",
+    "x-rapidapi-host": "spotify23.p.rapidapi.com",
+  },
+};
+
 export class API {
   constructor() {
     this.songs = [];
   }
 
-  searchMusic(query) {
-    console.log(query);
+  async searchMusic(query) {
+    try {
+      const res = await fetch(
+        `https://shazam.p.rapidapi.com/search?term=${query}&locale=en-US&offset=0&limit=5`,
+        options
+      );
+      const data = await res.json();
+      let newData = data.tracks.hits;
+
+      newData = newData.map((song) => ({ ...song.track }));
+      this.songs = newData;
+
+      renderSearchMusic(this.songs);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async topPopular() {
+    const url =
+      "https://spotify23.p.rapidapi.com/recommendations/?limit=20&seed_tracks=0c6xIDDpzE81m2q797ordA&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry";
+    try {
+      const res = await fetch(url, optionsTop);
+      const result = await res.json();
+
+      this.songs = result.tracks;
+      renderSongs(this.songs);
+    } catch (error) {}
   }
 }
--- main.js
+++ main.js
@@ -1,16 +1,52 @@
 import { API } from "./js/api.js";
 import { elements } from "./js/helpers.js";
+import { updateTitle, renderPlayingInfo } from "./js/ui.js";
 
 const api = new API();
 
 elements.form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(e);
-  const query = e.target(0).value;
+
+  const query = e.target[0].value;
 
   if (!query) {
     alert("Lütfen bir şarkı adı giriniz");
     return;
   }
+  updateTitle(`${query} İçin Sonuçlar`);
   api.searchMusic(query);
 });
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await api.topPopular();
+});
+
+const playMusic = (url) => {
+  elements.audioSource.src = url;
+
+  elements.audio.load();
+
+  elements.audio.play();
+};
+
+const handleClick = (e) => {
+  if (e.target.id === "play-btn") {
+    const parent = e.target.closest(".card");
+    renderPlayingInfo(parent.dataset);
+    playMusic(parent.dataset.url);
+  }
+};
+
+const animatePhoto = () => {
+  const img = document.querySelector(".info img");
+  img.classList = "animate";
+};
+const stopAnimation = () => {
+  const img = document.querySelector(".info img");
+  img.classList.remove("animate");
+};
+
+document.addEventListener("click", handleClick);
+
+elements.audio.addEventListener("play", animatePhoto);
+elements.audio.addEventListener("pause", stopAnimation);
